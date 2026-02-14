21ca9680a2b403dbea0dd8c71b85049f735772fa
Luís Cardoso <lfac-pt@users.noreply.github.com>
2026-02-14 21:43:39 +0000

    fix: Adds goat counter config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,13 +2,27 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 // Docs: https://rsbuild.rs/config/
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [pluginReact()],
   html: {
     title: 'Borboletas de Coimbra',
     favicon: './public/favicon.webp',
+    tags: isProd
+      ? [
+        {
+          tag: 'script',
+          attrs: {
+            'data-goatcounter': 'https://borboletas-de-coimbra.goatcounter.com/count',
+            async: true,
+            src: '//gc.zgo.at/count.js',
+          },
+        },
+      ]
+      : [],
   },
   output: {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/borboletas-de-coimbra/' : '/',
+    assetPrefix: isProd ? '/borboletas-de-coimbra/' : '/',
   },
 });
